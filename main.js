e83184dc5b2c3a4c36b305ff95ebee4255d76761
Princekashyap <vasukashyap08@gmail.com>
2023-02-08 20:24:39 +0530

    i complete that i also i do responsive work

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-const year =document.querySelector("#current-year")
-year.innerHTML = new Date().getFullYear(); 
-
+// Hamburger 
 let hamburger = document.querySelector(".hamburger");
 let navMenu = document.querySelector(".nav-menu");
 
@@ -10,3 +8,15 @@ function mobileMenu(){
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 }
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+};
+
+// copyRight 
+let year = document.querySelector("#current-year")
+year.innerHTML = new Date().getFullYear();
